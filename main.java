f3bd8885e867454230ed00da190adcba6a7b7e30
jho-ana <jhoanamaealvarez04@gmail.com>
2025-09-04 15:32:34 +0000

    hello world

---
+++ main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        
+        System.out.println("Jhoana");
+        int countdown= 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+       System.out.println ("Happy New Year!!");
+    }
+}
+    
